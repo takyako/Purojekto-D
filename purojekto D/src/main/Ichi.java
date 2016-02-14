4399e5007827201742f024a21027d5f19c4168b7
test <test@test.de>
2016-02-14 13:50:47 +0100

    button und layout

--- purojekto D/src/main/Ichi.java
+++ purojekto D/src/main/Ichi.java
@@ -1,6 +1,14 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+
+import net.miginfocom.swing.MigLayout;
 
 public class Ichi {
   
@@ -8,10 +16,29 @@ public class Ichi {
 		JFrame fenster = new JFrame() ;
 		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenster.setSize(800, 600);
+		fenster.getContentPane().setLayout(new MigLayout("", "[50px][50px,fill][][][][][]", "[50px][50px,fill][][][][][][]"));
+		
+		String test = "test";
+		JButton btn = new JButton("test");
 		
 		
-		
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String encryptedString = null;
+				MessageDigest messageDigest;
+				try {
+					messageDigest = MessageDigest.getInstance("SHA-256");
+					messageDigest.update(test.getBytes());
+					encryptedString = new String(messageDigest.digest());
+				} catch (NoSuchAlgorithmException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.out.println(encryptedString);
+			}
+		});
+		fenster.add(btn, "cell 1 1");
 		fenster.setVisible(true);
 		}
-	
 }
