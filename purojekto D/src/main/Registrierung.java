08fe4ddbbfb0ce0befb14ec7f6745bcb023e2d20
Hankyo <Hankyo>
2016-02-19 20:22:59 +0100

    registrierung fertig fürs erste

--- purojekto D/src/main/Registrierung.java
+++ purojekto D/src/main/Registrierung.java
@@ -15,7 +15,7 @@ import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 
 public class Registrierung {
-	
+
 	public Registrierung() {
 		JDialog dlgRegistrierung = new JDialog();
 		dlgRegistrierung.setModal(true);
@@ -31,29 +31,43 @@ public class Registrierung {
 		
 		JButton btnBestaetigen = new JButton("Registrieren");
 		JButton btnAbbrechen = new JButton("Abbrechen");
-		
+
 		
 		Action action = new AbstractAction()
 		{
 			private static final long serialVersionUID = 1L;
 			@Override
 		    public void actionPerformed(ActionEvent e)
-		    {
-		        System.out.println("some action");
+			{
+				System.out.println("Some action");
+
+				if (txtBenutzername.getText().length() >= 4) {
+
+					if (String.valueOf(txtPassword).length() >= 6) {
+
+						if ( String.valueOf(txtPassword.getPassword()).equals(String.valueOf(txtPassword2.getPassword())) ) {
+
+							int ret = Jgame.insertUser(txtBenutzername.getText(), String.valueOf(txtPassword.getPassword()));
+							if (ret > 0) {
+								JOptionPane.showMessageDialog(null, txtBenutzername.getText() + " wurde erfolgreich angelegt.", "Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
+								dlgRegistrierung.dispose();
+							} else {
+								JOptionPane.showMessageDialog(null, "Fehler beim anlegen von " + txtBenutzername.getText() + ".", "Kein Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Die Passwörter stimmen nicht überein.", "Kein Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Das Password muss mindestens 6 Zeichen lang sein.", "Kein Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Der Benutzername muss mindestens 4 Zeichen lang sein.", "Kein Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
+				}
 		    }
 		};
 		
 		
-		btnBestaetigen.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				int ret = Jgame.insertUser(txtBenutzername.getText(), txtPassword.getText());
-				if (ret > 0) {
-					JOptionPane.showMessageDialog(null, txtBenutzername.getText() + " wurde erfolgreich angelegt", "Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
-				} else
-					JOptionPane.showMessageDialog(null, "Fehler beim anlegen von " + txtBenutzername.getText(), "Kein Erfolg!!! (oder so)", JOptionPane.PLAIN_MESSAGE, null);
-			}
-		});
+		btnBestaetigen.addActionListener(action);
 		
 		btnAbbrechen.addActionListener(new ActionListener() {
 			@Override
@@ -76,10 +90,10 @@ public class Registrierung {
 
 		
 		dlgRegistrierung.getContentPane().add(btnBestaetigen, "cell 1 6");
-		btnBestaetigen.addActionListener(action);
+//		btnBestaetigen.addActionListener(action); //sonst wird das drei mal ausgeführt
 
 		dlgRegistrierung.getContentPane().add(btnAbbrechen, "cell 3 6");
-		btnBestaetigen.addActionListener(action);
+//		btnBestaetigen.addActionListener(action); //sonst wird das drei mal ausgeführt
 		
 		dlgRegistrierung.pack();
 		dlgRegistrierung.setVisible(true);
