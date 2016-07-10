3628e87f9373a045724ca777394cc48b19be3354
takyako <pspfreak3000@gmx.de>
2016-07-10 21:10:09 +0200

    Added functions to character

--- purojekto D/src/main/Charakter.java
+++ purojekto D/src/main/Charakter.java
@@ -1,8 +1,53 @@
 package main;
 
+import java.awt.Dimension;
+
 public class Charakter {
 
 	private String name;
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Dimension getC() {
+		return c;
+	}
+
+
+	public void setC(Dimension c) {
+		this.c = c;
+	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+
+	public int getAp() {
+		return ap;
+	}
+
+
+	public void setAp(int ap) {
+		this.ap = ap;
+	}
+
+
+	private Dimension c;
+	private int hp;
+	private int ap;
 
 
 	public Charakter(String name) {
