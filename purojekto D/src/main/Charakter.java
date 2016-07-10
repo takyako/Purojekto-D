ca388f684501139ab6ccf338829fbd4095a14e31
takyako <pspfreak3000@gmx.de>
2016-07-10 21:00:18 +0200

    Added character and field class

---
+++ purojekto D/src/main/Charakter.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Charakter {
+
+	private String name;
+
+
+	public Charakter(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
---
+++ purojekto D/src/main/Spielfeld.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.LinkedList;
+
+public class Spielfeld {
+
+	private String name;
+	private LinkedList<Charakter> chars;
+
+
+	public Spielfeld(String name) {
+		this.name = name;
+	}
+	
+	public void addCharakter(Charakter gamer){
+		chars.add(gamer);
+	}
+
+}
